d863f3d7a957a38a8bf8c75f3f7ec66aeeb1e577
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-06-03 13:18:48 +0330

    add: implementation middleware for auth service route

---
+++ pkg/auth/middleware.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/j3yzz/snapbuy-api-gateway/pkg/auth/pb"
+	"net/http"
+	"strings"
+)
+
+type AuthMiddlewareConfig struct {
+	svc *ServiceClient
+}
+
+func InitAuthMiddleware(svc *ServiceClient) AuthMiddlewareConfig {
+	return AuthMiddlewareConfig{svc}
+}
+
+func (m *AuthMiddlewareConfig) AuthRequired(ctx *gin.Context) {
+	authorization := ctx.Request.Header.Get("Authorization")
+
+	if authorization == "" {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	token := strings.Split(authorization, "Bearer ")
+	if len(token) < 2 {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	res, err := m.svc.Client.Validate(context.Background(), &pb.ValidateRequest{
+		Token: token[1],
+	})
+
+	if err != nil || res.Status != http.StatusOK {
+		ctx.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	ctx.Set("userId", res.UserId)
+
+	ctx.Next()
+}
