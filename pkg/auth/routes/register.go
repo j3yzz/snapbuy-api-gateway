270d4f65070d5c5e3b2cc0fdf617bff11a982305
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-06-03 12:37:21 +0330

    add: add register route

---
+++ pkg/auth/routes/register.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/j3yzz/snapbuy-api-gateway/pkg/auth/pb"
+	"net/http"
+)
+
+type RegisterRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Register(ctx *gin.Context, c pb.AuthServiceClient) {
+	body := RegisterRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := c.Register(context.Background(), &pb.RegisterRequest{
+		Email:    body.Email,
+		Password: body.Password,
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(int(res.Status), &res)
+}
