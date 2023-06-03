2be85382af3c5da414b165bca33c62fbe5c37dc2
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-06-03 12:55:43 +0330

    add: add login route

---
+++ pkg/auth/routes/login.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/j3yzz/snapbuy-api-gateway/pkg/auth/pb"
+	"net/http"
+)
+
+type LoginRequestBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func Login(ctx *gin.Context, c pb.AuthServiceClient) {
+	body := LoginRequestBody{}
+
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := c.Login(context.Background(), &pb.LoginRequest{
+		Email:    body.Email,
+		Password: body.Password,
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, &res)
+}
