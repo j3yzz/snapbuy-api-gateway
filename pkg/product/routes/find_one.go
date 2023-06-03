0530fb13e2b7f791b055e4b6da87ffe275287f47
Amirreza Kheradmand <amir.rezakheradmand.2017@gmail.com>
2023-06-03 14:05:55 +0330

    add: add create_product.go and find_one.go route

---
+++ pkg/product/routes/find_one.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/j3yzz/snapbuy-api-gateway/pkg/product/pb"
+	"net/http"
+	"strconv"
+)
+
+func findOne(ctx *gin.Context, c pb.ProductServiceClient) {
+	id, _ := strconv.ParseInt(ctx.Param("id"), 10, 32)
+
+	res, err := c.FindOne(context.Background(), &pb.FindOneRequest{
+		Id: int64(id),
+	})
+
+	if err != nil {
+		ctx.AbortWithError(http.StatusBadGateway, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, &res)
+}
